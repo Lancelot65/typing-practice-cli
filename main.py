8427f026d8f49583ac978e2834ab6092f0851ecd
lancelot.bulte <lancelot.bulte@monlycee.net>
2024-12-10 10:14:16 +0100

    v1.0 simple typing test without data info

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import curses
 from time import perf_counter
 import random
-
+from collections import defaultdict
 
 class database:
     def __init__(self):
@@ -10,48 +10,34 @@ class database:
 
     def load_word(self):
         with open ('frequency.txt', 'r', encoding='utf8') as file:
-            self.words = file.read().split('\n')
+            self.words = file.read().splitlines()
     
     def count_most_use_letter(self):
-        letter_count = {}
+        letter_count = defaultdict(int)
 
         for word in self.words:
             for letter in word:
-                if word in letter_count:
-                    letter_count[letter] += 1
-                else:
-                    letter_count[letter] = 1
+                letter_count[letter] += 1
+
         
         self.most_frequent_letter = list(dict(sorted(letter_count.items(), key=lambda item: item[1], reverse=True)))
     
     def get_word(self, letters : list, nbr_word : int) -> list:
-        lettres_set = set(letters)
-        mots_trouves = []
+        letters_set = set(letters)
 
-        for mot in self.words:
-            if set(mot).issubset(lettres_set):
-                mots_trouves.append(mot)
-        random.shuffle(mots_trouves)
-        return mots_trouves[:nbr_word]
+        mots_trouves = [mot for mot in self.words if set(mot).issubset(letters_set)]
+        
+        return random.sample(mots_trouves, min(nbr_word, len(mots_trouves)))
         
 class kyboard:
     def __init__(self):
-        self.new_space = '•'
         self.position = 0
-        self.sentence = "voici•ma•phrase"
         
         self.past_letter = []
-        self.time_letter_liste = []
-    
-        self.start_time = None
-        self.time_letter = None       
-        
-        
-        
-        
+            
         self.db = database()
         
-        self.setting = {'nbr_word' : 30, 'nbr_letter' : 10}
+        self.setting = {'nbr_word' : 3, 'nbr_letter' : 10}
         self.sentence = self.db.get_word(self.db.most_frequent_letter[:self.setting['nbr_letter']], self.setting['nbr_word'])
         self.sentence = "•".join(self.sentence)
         
@@ -65,54 +51,46 @@ class kyboard:
         curses.start_color()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)  # error typed text
         curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)  # Current character
-        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLACK)  # good character
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLACK)  # good typed text
     
     def print(self):
-        self.stdscr.clear()  # Clear the screen
-        # Display the current state
+        self.stdscr.clear()
+        # Display
         for i in range(len(self.sentence[:self.position])):
-            self.stdscr.addstr(0, i, self.sentence[i], curses.color_pair(3) if self.past_letter[i] else curses.color_pair(1))  # Correctly typed
-        self.stdscr.addstr(0, self.position, self.sentence[self.position], curses.color_pair(2))  # Current character
-        self.stdscr.addstr(0, self.position + 1, self.sentence[self.position + 1:])  # Remaining characters
-        self.stdscr.refresh()  # Refresh the screen to show changes
+            self.stdscr.addstr(0, i, self.sentence[i], curses.color_pair(3) if self.past_letter[i] else curses.color_pair(1))
+        self.stdscr.addstr(0, self.position, self.sentence[self.position], curses.color_pair(2))
+        self.stdscr.addstr(0, self.position + 1, self.sentence[self.position + 1:])
+        self.stdscr.refresh()
     
     def kyboard(self):
         key = self.stdscr.getch()
         if self.first_letter:
-            self.start_time = perf_counter()
-            self.time_letter = perf_counter()
-            first_letter = False
+            self.first_letter = False
 
         if key == ord(' '):
             if self.sentence[self.position] == '•':
                 self.position += 1
-                if len(self.past_letter) == self.position:
-                    pass
-                else:
+                if len(self.past_letter) != self.position:
                     self.past_letter.append(True)
-                    self.time_letter_liste.append(perf_counter() - self.time_letter)
-                    self.time_letter = perf_counter()
+
                 
         elif chr(key) == self.sentence[self.position]:  # Check for correct character
             self.position += 1
-            if len(self.past_letter) == self.position:
-                pass
-            else:
+            if len(self.past_letter) != self.position:
                 self.past_letter.append(True)
-                self.time_letter_liste.append(perf_counter() - self.time_letter)
-                self.time_letter = perf_counter()
+
         elif key == 27:
             self.position = 0
             self.past_letter.clear()
-            self.time_letter_liste.clear()
             self.first_letter = True
         else:
-            self.past_letter.append(False)
+            if len(self.past_letter) == self.position:
+                self.past_letter.append(False)
     
     def loop(self):
         self.first_letter = True
         while True:
-            self.print()
+            self.print()                
                     
             self.kyboard()
 
@@ -121,12 +99,5 @@ class kyboard:
 
 
         self.stdscr.erase()
-        
-        
-# dt = database()
-
-# dt.load_word()
-# dt.count_most_use_letter()
-# print(dt.get_word(dt.most_frequent_letter[:8], 100))
 
 kyboard()
