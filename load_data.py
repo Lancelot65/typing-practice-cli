2a4c0be5b6fca93ee9b03d36449c1e338bcf03bf
lancelot.bulte <lancelot.bulte@monlycee.net>
2024-12-13 00:53:22 +0100

    v1.2.1 update principal_print start collect data and use data

--- load_data.py
+++ load_data.py
@@ -1,24 +1,32 @@
 from json import load 
     
-class load_data:
+class LoadData:
     PATH_DICT = {
     'english' : 'data/english_word.json',
-    'frensh' : 'data/frensh_word.json',
+    'french' : 'data/french_word.json',
     'spanish' : 'data/spanish_word.json'
     }
     
-    def __init__(self, language = 'frensh') -> None:
+    def __init__(self, language = 'french') -> None:
         """
         Args:
             language (str, optional): change the language of words. Defaults to 'frensh'.
                 option : english, frensh, spanish
         """
+        if language not in self.PATH_DICT:
+            raise ValueError(f"Language '{language}' is not supported. Choose from {list(self.PATH_DICT.keys())}.")
         self.language = language
         self._load()
     
     def _load(self) -> None:
-        with open(self.PATH_DICT[self.language], 'r', encoding='utf-8') as file:
-            self.data = load(file)
+        try:
+            with open(self.PATH_DICT[self.language], 'r', encoding='utf-8') as file:
+                self.data = load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {self.PATH_DICT[self.language]}")
+        except ValueError as e:
+            raise ValueError(f"Error loading JSON: {e}")
+        
 
     def get_data(self):
         return self.data
--- main.py
+++ main.py
@@ -1,49 +1,52 @@
 import curses
-from time import time
+from time import time, sleep
 import random
-from collections import defaultdict
-from load_data import load_data
+from collections import defaultdict, Counter
+from load_data import LoadData
+from dataclasses import dataclass
 
 ESPACE = '•'
 
-class data_tools:
+class PrepareData:
     def __init__(self, words):
         self.words = words
         self.count_most_use_letter()
 
     def count_most_use_letter(self):
-        letter_count = defaultdict(int)
-
-        for word in self.words:
-            for letter in word:
-                letter_count[letter] += 1
-
-        self.most_frequent_letter = list(dict(sorted(letter_count.items(), key=lambda item: item[1], reverse=True)))
+        temp = Counter(''.join(self.words))        
+        self._most_frequent_letter = [letter for letter, _ in temp.most_common()]
     
     def get_word(self, nbr_letters : int, nbr_word : int) -> list:
-        letters_set = set(self.most_frequent_letter[:nbr_letters])
+        letters_set = set(self._most_frequent_letter[:nbr_letters])
 
-        mots_trouves = [mot for mot in self.words if set(mot).issubset(letters_set)]
+        mots_trouves = [mot for mot in self.words if all(letter in letters_set for letter in mot)]
         
         return random.sample(mots_trouves, min(nbr_word, len(mots_trouves)))
 
-class time_information:
-    def __init__(self, sentence : str):
-        self.time_session = None
-        self.time_by_letter = []
-        
-        self.letter_time = None
-        
-        self.sentence = sentence
+@dataclass
+class CollectData:
+    time_session = None
+    time_by_letter = []
+    who_is_false = []
     
+    buffer_false_letter = []
+    
+    letter_time = None
+        
     def start_letter(self):
         self.letter_time = time()
     
     def end_letter(self):
         self.time_by_letter.append(time() - self.letter_time)
+        self.who_is_false.append(self.buffer_false_letter.copy())
+        self.buffer_false_letter.clear()
         self.letter_time = time()
+        
+    def add_buffer(self, letter : str):
+        self.buffer_false_letter.append(letter)
     
     def start_test(self):
+        self.reset()
         self.time_session = time()
         self.start_letter()
     
@@ -54,25 +57,20 @@ class time_information:
     def reset(self):
         self.__init__()
     
-    def get_wpm_word(self):
-        return (len(self.sentence.split(ESPACE)) / (self.time_session/ 60))
+    def get_data(self):
+        return self.time_session, self.time_by_letter, self.who_is_false
     
-    def mean_wpm_letter(self):
-        return sum(self.time_by_letter) / len(self.time_by_letter)
-
-class kyboard:
+class Keyboard:
     def __init__(self):
         self.position = 0
         
         self.past_letter = []
-            
-        self.setting = {'nbr_word' : 200, 'nbr_letter' : 20}
-        self.data = load_data().data
-        self.data_tools = data_tools(self.data)
-        self.sentence = self.data_tools.get_word(self.setting['nbr_letter'], self.setting['nbr_word'])
+        
+        self.setting = {'nbr_word' : 3, 'nbr_letter' : 15}
+        self.sentence = PrepareData(LoadData().data).get_word(self.setting['nbr_letter'], self.setting['nbr_word'])
         self.sentence = "•".join(self.sentence)
         
-        self.time_info = time_information(self.sentence)
+        self.collect_data = CollectData()
         
         self.init_screen()
         self.loop()
@@ -84,19 +82,19 @@ class kyboard:
         curses.start_color()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)  # error typed text
         curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)  # Current character
-        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLACK)  # good typed text
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # good typed text
 
-    def print(self): # marche plus
+    def print_classique(self):
         height, width = self.stdscr.getmaxyx()
         rang, col, i = 0, 0, 0
         
         def print_char(letter):
+            color = curses.color_pair(3)
             if i < len(self.past_letter):
-                self.stdscr.addstr(rang, col, letter, curses.color_pair(3) if self.past_letter[i] else curses.color_pair(1))
-            else:
-                self.stdscr.addstr(rang, col, letter, curses.color_pair(3))
+                color = curses.color_pair(3) if self.past_letter[i] else curses.color_pair(1)
             if i == self.position:
-                self.stdscr.addstr(rang, col, letter, curses.color_pair(2))
+                color = curses.color_pair(2)
+            self.stdscr.addstr(rang, col, letter, color)
 
         liste_words = self.sentence.split(ESPACE)
         for word in liste_words:
@@ -123,8 +121,8 @@ class kyboard:
             i += 1
                 
         self.stdscr.refresh()
-        
-    def print_float(self, _front_visibility, _back_visibility, start_col = 5): # print center        
+    
+    def print_move(self, _front_visibility, _back_visibility, start_col = 5): # print center        
         front_visibility = _front_visibility
         back_visibility = _back_visibility
         
@@ -158,31 +156,35 @@ class kyboard:
             col += 1
 
         self.stdscr.refresh()
-
-    
+ 
     def kyboard(self):
         key = self.stdscr.getch()
         if self.first_letter:
             self.first_letter = False
-            self.time_info.start_test()
+            self.collect_data.start_test()
 
         if key == ord(' '):
             if self.sentence[self.position] == ESPACE:
                 self.position += 1
                 if len(self.past_letter) != self.position:
                     self.past_letter.append(True)
-                    self.time_info.end_letter()
-                    self.time_info.start_letter()
+                self.collect_data.end_letter()
+                if self.position < len(self.sentence):
+                    self.collect_data.start_letter()
+            else:
+                if len(self.past_letter) == self.position:
+                    self.past_letter.append(False)
+                self.collect_data.add_buffer(' ')
 
                 
         elif chr(key) == self.sentence[self.position]:  # Check for correct character
             self.position += 1
             if len(self.past_letter) != self.position:
                 self.past_letter.append(True)
-                self.time_info.end_letter()
-                
-                if self.position < len(self.sentence):
-                    self.time_info.start_letter()
+            self.collect_data.end_letter()
+        
+            if self.position < len(self.sentence):
+                self.collect_data.start_letter()
 
         elif key == 27:
             self.run = False
@@ -193,22 +195,65 @@ class kyboard:
         else:
             if len(self.past_letter) == self.position:
                 self.past_letter.append(False)
-    
+            self.collect_data.add_buffer(chr(key))
+        
     def loop(self):
         self.first_letter = True
         self.run = True
         while self.run:
-            # self.print_flaot('auto', 0)                
-            self.print()
+            self.print_move(25, 10, 0) 
             self.kyboard()
 
             if self.position >= len(self.sentence):
-                self.time_info.end_test()
+                self.collect_data.end_test()
                 break
-
-    def __del__(self):
-        self.stdscr.erase()
         
-ky = kyboard()
-print(ky.time_info.get_wpm_word())
+t = Keyboard()
+
+class Stat:
+    def __init__(self, info : tuple , sentence): # (time_session, time_by_letter, who_is_false)
+        self.time_session = info[0]
+        self.time_by_letter = info[1]
+        self.who_is_false = info[2]
+        
+        self.sentence = sentence
+        
+        self.clean_start()
     
+    def clean_start(self):
+        self.time_by_letter.pop(0)
+    
+    def get_wpm_word(self):
+        return (len(self.sentence.split(ESPACE)) / (self.time_session / 60))
+    
+    def mean_wpm_letter(self):
+        return sum(self.time_by_letter) / len(self.time_by_letter)
+    
+    def wpm_by_letter(self):
+        unique_time_letter = {}
+        for i in range(len(self.time_by_letter) - 1):
+            # ajouter 1 quand on appelle sentene parce que on a pop(0)
+            if self.sentence[i + 1] not in unique_time_letter:
+                unique_time_letter[self.sentence[i + 1]] = [self.time_by_letter[i]]
+            else:
+                unique_time_letter[self.sentence[i + 1]].append(self.time_by_letter[i])
+        
+        self.mean_time_by_letter = {}
+        for i in unique_time_letter:
+            self.mean_time_by_letter[i] = sum(unique_time_letter[i]) / len(unique_time_letter[i])
+        
+        return (self.mean_time_by_letter)
+
+    def count_all_error(self):
+        return len(self.who_is_false) - self.who_is_false.count([])
+    
+    # rajouter quel lettre sont trompe et confondu avec lesquelle ?
+
+stat = Stat(t.collect_data.get_data()[0], t.collect_data.get_data()[1], t.sentence)
+print(t.collect_data.who_is_false)
+
+
+# TODO
+# [ ] add history value
+# [ ] traiter les erreurs dans stat (total, pourcentage d'erreur par lettre)
+# [ ] faire un affichage pour les stats
